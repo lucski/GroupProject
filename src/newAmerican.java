1382364a4ff08b5c3bb29b35155a7f71dd4af2cb
lucski <162204475+lucski@users.noreply.github.com>
2024-05-16 21:38:28 -0500

    Update newAmerican.java

--- src/newAmerican.java
+++ src/newAmerican.java
@@ -1,2 +1,62 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class newAmerican {
+
+    public static void americanDisplay() {
+        Scanner scanner = new Scanner(System.in);
+
+        // Display the list of American recipes
+        System.out.println("American Recipes:");
+        List<String> americanRecipes = newRecipe.cuisines.get("American");
+        for (int i = 0; i < americanRecipes.size(); i++) {
+            System.out.println((i + 1) + ". " + americanRecipes.get(i));
+        }
+
+        // Prompt the user to enter a number corresponding to a recipe
+        System.out.println("Enter the number of the recipe you want to check:");
+        int recipeNumber = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        // Check if the entered number is within the valid range
+        if (recipeNumber < 1 || recipeNumber > americanRecipes.size()) {
+            System.out.println("Invalid recipe number. Please try again.");
+            return;
+        }
+
+        // Get the recipe corresponding to the entered number
+        String selectedRecipe = americanRecipes.get(recipeNumber - 1);
+
+        // Check if "Burgers" is the selected recipe
+        if (selectedRecipe.equals("Burgers")) {
+            System.out.println("You selected Burgers!");
+            System.out.println("The Recipe is:\n" +
+                    "▢ 1 lb ground beef\n" +
+                    "▢ 1/2 tsp salt\n" +
+                    "▢ 1/2 tsp black pepper\n" +
+                    "▢ 1/2 tsp garlic powder\n" +
+                    "▢ 1/2 tsp onion powder\n" +
+                    "▢ 4 hamburger buns\n" +
+                    "▢ Optional toppings: lettuce, tomato, cheese, pickles, onions, ketchup, mustard");
+
+         
+        }
+
+        // Additional recipes can be added here in a similar manner
+        // Example for "Fried Chicken"
+        if (selectedRecipe.equals("Fried Chicken")) {
+            System.out.println("You selected Fried Chicken!");
+            System.out.println("The Recipe is:\n" +
+                    "▢ 1 whole chicken, cut into pieces\n" +
+                    "▢ 2 cups buttermilk\n" +
+                    "▢ 2 cups all-purpose flour\n" +
+                    "▢ 1 tsp paprika\n" +
+                    "▢ 1 tsp garlic powder\n" +
+                    "▢ 1 tsp salt\n" +
+                    "▢ 1/2 tsp black pepper\n" +
+                    "▢ Vegetable oil for frying");
+
+            
+        }
+    }
 }
